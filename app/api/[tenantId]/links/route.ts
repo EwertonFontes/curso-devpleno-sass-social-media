4e276d87bdead84fd58d6c675769ed96aec85b01
Ewerton Fontes <ewerton.p.fontes@gmail.com>
2025-01-22 20:53:34 -0300

    adicionando links aula 11

--- app/api/[tenantId]/links/route.ts
+++ app/api/[tenantId]/links/route.ts
@@ -1,23 +1,25 @@
 
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 import { prisma } from "lib/prisma"
 import { getSession } from "next-auth/react"
 import { getServerSession } from "next-auth"
 import { authOptions } from "../../auth/[...nextauth]/route"
+import type { NextApiRequest } from "next"
 
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {    
+export async function POST(req: NextRequest, { params }: { params: Promise<{ tenantId: string }> }) {  
     const session = await getServerSession(authOptions)
-    
     if (session) {
-        console.log('REQUEST DONE')
+        const body = await req.json()
+        const tenantId = (await params).tenantId;
         const linkData = {
-            name: req.body.name,
-            publicName: req.body.publicName,
-            slug: req.body.slug,
-            destination: req.body.destination,
-            tenantId: req.body.tenantId
+            name: body.name,
+            publicName: body.publicName,
+            slug: body.slug,
+            destination: body.destination,
+            tenantId: tenantId
         }
+
         const tenants = await prisma.tenant.findMany({
             where: {
                 users: {
@@ -27,28 +29,29 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
                 }
             }
         })
-
+        
         const savedLink = await prisma.link.create({
             data: linkData
         })
-        return NextResponse.json(tenants, { status: 200  })
+
+        return NextResponse.json(savedLink, { status: 200  })
     } else {
         return NextResponse.json('ERRORS', { status: 404 })
     }
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {    
+export async function GET(req: NextApiRequest, { params }: { params: Promise<{ tenantId: string }> }) {
     const session = await getServerSession(authOptions)
-    
     if (session) {
+        const tenantId = (await params).tenantId;
+    
         const links = await prisma.link.findMany({
             where: {
                 tenantId: {
-                    equals: req.query.tenantId
+                    equals: tenantId
                 }
             }
         })
-
         return NextResponse.json(links, { status: 200  })
     } else {
         return NextResponse.json('ERRORS', { status: 404 })
